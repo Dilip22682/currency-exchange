4e4c8e5386dda5e136e3cf5cec0abc3ef9db69dd
Dilip22682 <princekp232101@gmail.com>
2026-08-04 16:56:22 +0530

    Prepare for PythonAnywhere deployment

--- money_exchange/settings.py
+++ money_exchange/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,10 +28,13 @@ DEBUG = True
 
 # ALLOWED_HOSTS = []
 ALLOWED_HOSTS = [
-    "localhost",
-    "127.0.0.1",
-    ".onrender.com",
+    'Diliphostedcode2026.pythonanywhere.com',
+    'Diliphostedcode2026.eu.pythonanywhere.com'
 ]
+
+DEBUG = False  # set to False for production
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Application definition
